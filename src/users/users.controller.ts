87e3a3c44c1bf9d74204cd1f1bfca5b572409d30
UzairCodes2004 <raouzair70@gmail.com>
2026-06-29 18:52:04 +0500

    make that slub go awau

---
+++ src/users/users.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+
+// it will handle users routes
+@Controller('users')
+export class UsersController {
+    // routes we want to handle below
+
+    /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+
+    // GET /users
+    @Get()
+    findAll() {
+        return [];
+    }
+
+    // GET /users/:id anything after user/ or params any get route will be read as user/interns and the value will be stored in get
+    @Get(':id')
+    findOne(@Param('id') id :string) {
+        return { id };
+    }
+
+    // POST route which post data from the body and user is the type
+    @Post() create(@Body() user:{}){
+        return user
+    }
+
+    @Patch(':id')
+    update(@Param ()id :string){
+        return {id}
+     }
+}
+
