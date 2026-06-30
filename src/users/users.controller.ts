1e3faf18e71e9931a5c2a15766876558089c18f9
UzairCodes2004 <raouzair70@gmail.com>
2026-06-30 14:43:07 +0500

    Implemented logic in users service and injected them in respective controller methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, } from '@nestjs/common';
 
-
+import { UsersService } from './users.service';
 // it will handle users routes
 @Controller('users')
 export class UsersController {
@@ -26,30 +26,36 @@ export class UsersController {
     // QUERY PARAMS
     // GET /users?role=admin
 
+    // Constructor handles the user servicce does same as creating a constructor but if we have created it else where it will fetch that too
+
+    constructor(private readonly userService: UsersService) { }
     @Get()
     findAll(@Query('role') role?: 'INTERN' | 'ASE' | 'SSE') {
-        return 'Query params checked Role is : ' + role;
+        return this.userService.findAll(role)
     }
 
     // GET /users/:id anything after user/ or params any get route will be read as user/interns and the value will be stored in get
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return { id };
+        const uId = parseInt(id)
+        return this.userService.findOne(uId);
     }
 
     // POST route which post data from the body and user is the type
-    @Post() create(@Body() user: {}) {
-        return user
+    @Post() create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ASE' | 'SSE' }) {
+        return this.userService.create(user)
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate };
+    update(@Param('id') id: string, @Body() userUpdate: { name: string, email: string, role: 'INTERN' | 'ASE' | 'SSE' }) {
+        const uId = parseInt(id)
+        return this.userService.update(uId, userUpdate);
     }
 
     @Delete(':id')
     delete(@Param('id') id: string) {
-        return { id };
+        const uId = parseInt(id)
+        return this.userService.delete(uId);
     }
 }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users = [
+        { id: 1, name: 'Alice Johnson', email: 'alice@example.com', role: 'INTERN' },
+        { id: 2, name: 'Bob Smith', email: 'bob@example.com', role: 'ASE' },
+        { id: 3, name: 'Carol White', email: 'carol@example.com', role: 'SSE' },
+        { id: 4, name: 'David Brown', email: 'david@example.com', role: 'ASE' },
+        { id: 5, name: 'Eve Davis', email: 'eve@example.com', role: 'INTERN' },
+    ]
+    // GET users or Get users through query params users?role=
+    findAll(role?: 'INTERN' | 'ASE' | 'SSE') {
+        const user = this.users;
+        if (role) {
+
+            const filteredUsers = user.filter(user => user.role === role)
+            return filteredUsers;
+        }
+        return user;
+
+    }
+    // Get user/:id
+    findOne(id: number) {
+        const user = this.users;
+        const userById = user.filter(user => user.id === id)
+        return userById;
+    }
+
+    // Post users
+
+    create(user: { name: string, email: string, role: 'INTERN' | 'ASE' | 'SSE' }) {
+        const userByHighestId = [...this.users].sort((a, b) => b.id - a.id)
+
+        const newUser = {
+
+            id: userByHighestId[0].id + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser;
+    }
+    // PATCH method users/:id
+    update(id: number, updatedUser: { name: string, email: string, role: 'INTERN' | 'ASE' | 'SSE' }) {
+    
+    this.users = this.users.map(user => {
+        if (user.id === id) {
+            return { ...user, ...updatedUser }; 
+        }
+        return user; 
+    });
+
+    return this.findOne(id);
+}
+// Delete method 
+delete(id:number){
+    const removedUser=this.findOne(id)
+    this.users = this.users.filter(user=>user.id!==id);
+     
+   return removedUser;
+}
+}
+
+
